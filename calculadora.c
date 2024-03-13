146b8557d693f2e5eea3c04214fe096f12f9d600
GuilhermeAFigueiredo <157835797+GuilhermeAFigueiredo@users.noreply.github.com>
2024-03-13 19:01:43 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -4,14 +4,12 @@ int main() {
     char operador; 
     double num1, num2, resultado;
 
-    // Solicita ao usuário que insira o operador e dois números
     printf("Digite um operador (+, -, *, /): ");
     scanf("%c", &operador);
 
     printf("Digite dois números: "); 
     scanf("%lf %lf", &num1, &num2);
 
-    // Realiza a operação com base no operador fornecido
     switch (operador) {
         case '+':
             resultado = num1 + num2;
@@ -27,15 +25,14 @@ int main() {
                 resultado = num1 / num2;
             } else {
                 printf("Erro: Divisão por zero!\n");
-                return 1; // Retorna 1 para indicar um erro
+                return 1; 
             }
             break;
         default:
             printf("Operador inválido!\n");
-            return 1; // Retorna 1 para indicar um erro
+            return 1; 
     }
 
-    // Exibe o resultado da operação
     printf("Resultado: %.2lf\n", resultado);
 
     return 0;
